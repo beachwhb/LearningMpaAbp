ceb9176ed645377a4116379e0dc7c9946d857d8b
Ocean <beachwhb@qq.com>
2018-06-11 16:59:24 +0800

    自定义Task仓储接口&类

---
+++ LearningMpaAbp.Application/Tasks/TaskAppService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Application.Services;
+using Abp.Domain.Repositories;
+
+namespace LearningMpaAbp.Tasks
+{
+    public class TaskAppService : IApplicationService
+    {
+        private readonly IRepository<Task> _taskRepository;
+
+        public TaskAppService(IRepository<Task> taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public void GetAll()
+        {
+            var all = _taskRepository.Count();
+        }
+
+    }
+}
---
+++ LearningMpaAbp.Core/IRepositories/IBackendTaskRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Repositories;
+using Task = LearningMpaAbp.Tasks.Task;
+
+namespace LearningMpaAbp.IRepositories
+{
+    /// <summary>
+    /// 自定义仓储接口
+    /// </summary>
+    public interface IBackendTaskRepository : IRepository<Task>
+    {
+        /// <summary>
+        /// 根据受配人的ID，找出他所有的任务
+        /// </summary>
+        /// <param name="personId">受配人的ID</param>
+        /// <returns></returns>
+        List<Task> GetTaskByAssignedPersonId(long personId);
+    }
+}
